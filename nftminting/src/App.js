6206589e57d93dd7486fdad2e655ab306ab1e7c0
Mohammad Dakdouki <84288006+MD99-936@users.noreply.github.com>
2022-03-05 18:02:14 +0200

    Add files via upload

--- nftminting/src/App.js
+++ nftminting/src/App.js
@@ -6,6 +6,25 @@ import * as s from "./styles/globalStyles";
 import styled from "styled-components";
 
 
+const isMobileDevice = useMediaQuery({
+  query: "(min-device-width: 480px)",
+});
+
+const isTabletDevice = useMediaQuery({
+  query: "(min-device-width: 768px)",
+});
+
+const isLaptop = useMediaQuery({
+  query: "(min-device-width: 1024px)",
+});
+
+const isDesktop = useMediaQuery({
+  query: "(min-device-width: 1200px)",
+});
+
+const isBigScreen = useMediaQuery({
+  query: "(min-device-width: 1201px )",
+});
 
 export const StyledButton = styled.button`
 width: 260px;
@@ -221,6 +240,13 @@ function App() {
 
   return (
     <s.Screen>
+      {isMobileDevice && <Mobile />}
+    {isTabletDevice && <>
+    <TabletMobile />
+    {isDesktop && <Desktop />}
+    {isLaptop && <Laptop />}
+    {isBigScreen && <BigScreen />}
+    </>}
       <s.Container
         flex={1}
         ai={"center"}
