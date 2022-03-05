2e5cf9a6daa56b0987bccd89e205489408513341
Mohammad Dakdouki <84288006+MD99-936@users.noreply.github.com>
2022-03-05 15:46:32 +0200

    Add files via upload

--- nftminting/src/reportWebVitals.js
+++ nftminting/src/reportWebVitals.js
@@ -9,5 +9,27 @@ const reportWebVitals = onPerfEntry => {
     });
   }
 };
+const webpack = require("webpack");
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: [require.resolve("buffer/"), "Buffer"],
+            }),
+        ],
+        resolve: {
+            fallback: {
+                "crypto": false,
+                "stream": require.resolve("stream-browserify"),
+                "assert": false,
+                "util": false,
+                "http": false,
+                "https": false,
+                "os": false
+            },
+        },
+    })
+}
 
 export default reportWebVitals;
