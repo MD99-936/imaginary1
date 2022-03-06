2f321e5d41c427ac84371a6fc7eee6d6a143ad04
Mohammad Dakdouki <84288006+MD99-936@users.noreply.github.com>
2022-03-06 21:53:17 +0200

    Add files via upload

--- nftminting/src/App.js
+++ nftminting/src/App.js
@@ -188,8 +188,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount >= 2) {
-      newMintAmount = 2;
+    if (newMintAmount >= 10) {
+      newMintAmount = 10;
     }
     setMintAmount(newMintAmount);
   };
